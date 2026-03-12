47175c6cf90f49634d6eb585b2c09292119be669
Siro Devs <sirodevske@gmail.com>
2026-03-12 11:18:41 +0300

    finding the source of truth in MDX configs and page components

--- src/components/MDX/MDXComponents.tsx
+++ src/components/MDX/MDXComponents.tsx
@@ -315,7 +315,7 @@ function Illustration({
   return (
     <div className="relative group before:absolute before:-inset-y-16 before:inset-x-0 my-16 mx-0 2xl:mx-auto max-w-4xl 2xl:max-w-6xl">
       <figure className="my-8 flex justify-center">
-        <Image
+        <img
           src={src}
           alt={alt}
           style={{maxHeight: 300}}
@@ -351,7 +351,7 @@ function IllustrationBlock({
   const images = imageInfos.map((info, index) => (
     <figure key={index}>
       <div className="bg-white rounded-lg p-4 flex-1 flex xl:p-6 justify-center items-center my-4">
-        <Image
+        <img
           className="text-primary"
           src={info.src}
           alt={info.alt}
@@ -487,9 +487,7 @@ function YouTubeIframe(props: any) {
 
 function Image(props: any) {
   const {alt, ...rest} = props;
-  return (
-    <Image alt={alt} className="max-w-[calc(min(700px,100%))]" {...rest} />
-  );
+  return <img alt={alt} className="max-w-[calc(min(700px,100%))]" {...rest} />;
 }
 
 export const MDXComponents = {
